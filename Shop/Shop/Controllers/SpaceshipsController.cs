c319974bea2f7cbc125642ad1a653a6b6bc4f02c
Ingvar Derešivski <inde999@gmail.com>
2023-09-05 15:52:47 +0300

    SpaceshipCreate

---
+++ Shop/Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
