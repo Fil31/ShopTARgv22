c76a249104f6ea4a7e96f1319b2e94382bd529fe
Fil31 <101242957+Fil31@users.noreply.github.com>
2023-09-25 12:06:35 +0300

    HHHH

---
+++ Shop.Core/Domain/RealEstate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Domain
+{
+    public class RealEstate
+    {
+        public Guid Guid { set; get; }
+        public string Address { set; get; }
+        public float SizeSqrtM { get; set; }
+        public int RoomCount { get; set; }
+
+        public int Floor { get; set; }
+        public string BuildingType { get; set; }
+        public DateTime BuildInYear { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    
+    }
+}
